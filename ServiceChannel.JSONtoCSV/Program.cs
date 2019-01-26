12020c52176062fc20bbbfed18f900dd2226b00d
pliebscher <pliebscher@onelegal.com>
2019-01-26 10:28:39 -0800

    Initial commit.

--- ServiceChannel.JSONtoCSV/Program.cs
+++ ServiceChannel.JSONtoCSV/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ServiceChannel.JSONtoCSV
 {
@@ -10,6 +11,7 @@ namespace ServiceChannel.JSONtoCSV
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
